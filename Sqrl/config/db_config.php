64416c0c1d24b989a46cf5d1be8b823a77192bcd
Development <joseph.lee.esl@gmail.com>
2019-06-16 16:05:43 -0400

    changed the size of my nuts

--- Sqrl/config/db_config.php
+++ Sqrl/config/db_config.php
@@ -4,7 +4,7 @@ define("DB_APPLICATION", "mysql");
 define("DB_HOST", "localhost");
 define("DB_NAME", "sqrl_login");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "XOjRD*P8ymp0^r!%zrHzkcR%NyXGFE^rbYDn8QMP2w3eR@6hwNEXU17gOI5i");
+define("DB_PASSWORD", "5vsU9ndSgEd6skW5sTe");
 define("NONCE_TABLE", "sqrl_nonce");
 define("PUBKEY_TABLE", "sqrl_pubkey");
 
